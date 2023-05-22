ebac727b2f5c3c415f1d1091344ab445cb8da2e1
eyrond <admin@eyrond.dev>
2023-05-22 19:16:32 +0300

    Add permission related methods for LiteralArgument

--- core/src/main/kotlin/brigadier/dsl/LiteralArgument.kt
+++ core/src/main/kotlin/brigadier/dsl/LiteralArgument.kt
@@ -1,8 +1,11 @@
+@file:Suppress("unused")
+
 package dev.eyrond.paperkt.brigadier.dsl
 
 import com.destroystokyo.paper.brigadier.BukkitBrigadierCommandSource
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
 import dev.eyrond.paperkt.plugin.IKotlinPlugin
+import org.bukkit.permissions.Permission
 
 typealias BukkitLiteralArgumentBuilder = LiteralArgumentBuilder<BukkitBrigadierCommandSource>
 
@@ -14,4 +17,21 @@ typealias BukkitLiteralArgumentBuilder = LiteralArgumentBuilder<BukkitBrigadierC
 class LiteralArgument(
     plugin: IKotlinPlugin,
     builder: BukkitLiteralArgumentBuilder
-) : Argument<BukkitLiteralArgumentBuilder>(plugin, builder)
+) : Argument<BukkitLiteralArgumentBuilder>(plugin, builder) {
+
+    /**
+     * Sets a permission requirement for this argument.
+     */
+    @BrigadierDSLMarker
+    fun requiresPermission(permission: Permission) = apply {
+        builder.requires { it.bukkitSender.hasPermission(permission) }
+    }
+
+    /**
+     * Sets a permission requirement for this argument.
+     */
+    @BrigadierDSLMarker
+    fun requiresPermission(permission: String) = apply {
+        builder.requires { it.bukkitSender.hasPermission(permission) }
+    }
+}
